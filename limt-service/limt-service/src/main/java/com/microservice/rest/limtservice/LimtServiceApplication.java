daeb17b52f244dc88c4035f842bd226d798d5715
Mousam Saikia <mousam.javatechie@gmail.com>
2021-12-01 23:00:14 +0530

    frist micro v2

---
+++ limt-service/limt-service/src/main/java/com/microservice/rest/limtservice/LimtServiceApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.rest.limtservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimtServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimtServiceApplication.class, args);
+	}
+
+}
---
+++ limt-service/limt-service/src/test/java/com/microservice/rest/limtservice/LimtServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.rest.limtservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimtServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
